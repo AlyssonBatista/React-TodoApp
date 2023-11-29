8afab3ccc85c8c0649dcb69a9ade29a8fed1ea88
Alysson Batista de Souza <alyssonbatista241@gmail.com>
2023-11-29 02:03:13 -0300

    backend ok

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,15 +1,12 @@
 const port = 3003
-
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
-
-server.use(bodyParser.urlencoded({extended: true}))
+const allowCors = require('./cors')
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
-
-server.listen(port, function(){
-console.log((`Backend is ruinning on port ${port}`))
+server.use(allowCors)
+server.listen(port, function() {
+  console.log(`BACKEND is running on port ${port}.`)
 })
-
-
 module.exports = server
