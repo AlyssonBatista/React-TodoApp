f4e7c7d2d070c1f0ab1728e0f156fd0b18f23479
AlyssonBatista <alyssonbatista241@gmail.com>
2024-01-10 10:53:29 -0300

    add componets and routes

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,10 +2,13 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import Menu from '../template/Menu'
+import Routes from './Routes'
 
 
 export default props => (
     <div className='container'>
-        <h1>TodoApp</h1>
+        <Menu/>
+         <Routes/>
     </div>
 )
